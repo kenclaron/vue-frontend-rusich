28a6263d8e675c46547c479e62c346b2f45c3783
Dmitry Britov <da.britov@yandex.ru>
2022-10-19 18:59:15 +0300

    Convert .css to .scss via sass

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import vSelect from "vue-select";
 
-import "./assets/main.css";
+import "./assets/scss/main.scss";
 
 createApp(App).component("v-select", vSelect).mount("#app");
